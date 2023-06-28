d9137112d39de277ea46281603e53316447f5a1a
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-06-28 00:00:08 -0300

    feat: continues player controller

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,17 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 import { CreatePlayer } from './dtos/create-player.dto';
+import { PlayerService } from './player.service';
+import { Player } from './interfaces/player.interface';
 
 @Controller('api/v1/player')
 export class PlayerController {
+  constructor(private readonly playerService: PlayerService) {}
+
   @Get()
-  async index() {
-    return JSON.stringify({ nome: 'Alisson' });
+  async index(): Promise<Player[]> {
+    return this.playerService.findAll();
   }
 
   @Post()
   async create(@Body() player: CreatePlayer) {
-    const { name } = player;
-    return JSON.stringify({ message: `Welcome ${name}!` });
+    await this.playerService.save(player);
+  }
+
+  @Get('/:id')
+  async read(@Param() param: { id: string }): Promise<string> {
+    return JSON.stringify({ id: param.id });
   }
 }

431ebca0cb0bdd030228322c0074cfa584bcd4b5
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-07-29 18:56:31 -0300

    feat: fixes prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PlayerModule } from './modules/player/player.module';
-import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [PlayerModule],
   controllers: [],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}

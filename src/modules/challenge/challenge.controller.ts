9621b96cfd37f3ff7a3781bf857617c9b006cce5
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-08-09 23:46:32 -0300

    feat: adds index method

--- src/modules/challenge/challenge.controller.ts
+++ src/modules/challenge/challenge.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Challenge } from '@prisma/client';
 import { ChallengeService } from './challenge.service';
 import { CreateChallenge } from './dtos/create-challenge-dto';
+import { IndexValidatorQueryPipe } from 'src/shared/pipes/index-validator-query.pipe';
 
 @Controller('api/v1')
 export class ChallengeController {
   constructor(private readonly challengeService: ChallengeService) {}
+
+  @Get('/challenges/:id')
+  async index(
+    @Param() params: { id: string },
+    @Query(IndexValidatorQueryPipe) query,
+  ): Promise<Challenge[]> {
+    return await this.challengeService.findAll(params.id, query);
+  }
 
   @Post('/challenge')
   async create(@Body() challenge: CreateChallenge): Promise<void> {
--- src/modules/challenge/challenge.service.ts
+++ src/modules/challenge/challenge.service.ts
@@ -1,10 +1,29 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Challenge } from '@prisma/client';
 import { PrismaService } from 'src/shared/database/prisma/prisma.service';
 import { CreateChallenge } from './dtos/create-challenge-dto';
 
 @Injectable()
 export class ChallengeService {
   constructor(private prismaService: PrismaService) {}
+
+  async findAll(
+    id: string,
+    query: { page: number; size: number },
+  ): Promise<Challenge[]> {
+    const { page, size } = query;
+    return await this.prismaService.challenge.findMany({
+      take: size,
+      skip: (page - 1) * size,
+      where: {
+        PlayerChallenge: {
+          some: {
+            playerId: id,
+          },
+        },
+      },
+    });
+  }
 
   async save(challenge: CreateChallenge): Promise<void> {
     const { challengerId, challengedId, status, config } = challenge;

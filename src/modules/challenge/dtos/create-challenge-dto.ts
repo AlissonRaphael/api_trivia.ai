3da5b8598119425301df2e96e22575a2afa27898
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-08-11 22:53:04 -0300

    feat: starts question generator

--- src/modules/challenge/dtos/create-challenge-dto.ts
+++ src/modules/challenge/dtos/create-challenge-dto.ts
@@ -1,4 +1,5 @@
 import { IsNotEmpty } from 'class-validator';
+import { Difficulty } from '../types/difficulty';
 
 export class CreateChallenge {
   @IsNotEmpty()
@@ -11,5 +12,21 @@ export class CreateChallenge {
   status: string;
 
   @IsNotEmpty()
-  config: string;
+  difficulty: Difficulty;
+
+  @IsNotEmpty()
+  config: Config;
 }
+
+type Config = {
+  matches: number;
+  questions: Question[];
+  winner: string | null;
+  score: { challenged: number; challenger: number };
+};
+
+type Question = {
+  question: string;
+  answers: { a: string; b: string; c: string; d: string };
+  correctAlternative: number;
+};
---
+++ src/modules/challenge/types/difficulty.d.ts
@@ -0,0 +1,7 @@
+export enum Difficulty {
+  VeryHard = 'nível doutorado e pós-doutorado',
+  Hard = 'nível graduação universitária',
+  Normal = 'nível ensino médio',
+  Easy = 'nível ensino fundamental',
+  VeryEasy = 'nível ensino básico',
+}

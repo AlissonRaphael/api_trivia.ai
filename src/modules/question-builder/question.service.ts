53f9d5e7c8473b4cd9411e55ea779e9b20291e8e
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2023-08-16 23:30:59 -0300

    feat: improvements question-builder

--- src/modules/question-builder/question.service.ts
+++ src/modules/question-builder/question.service.ts
@@ -16,13 +16,21 @@ export class QuestionBuilderService {
   handle(config: QuestionBuilderConfig): string {
     const { difficulty, matches, themes } = config;
     const level = this.dificulty(difficulty);
-    return `Crie ${matches} perguntas com 4 alternativas enumeradas de A á B. Cada pergunta deve ter no máximo 45 tokens. Cada alternativa deve ter no máximo 20 tokens. As perguntas devem ter o nível ${level}. Os temas das perguntas devem ser: ${themes.join(', ')}. Não retorne NADA. APENA um array de objetos Javascript na estrutura: ${OBJECT}`
+    return `Crie ${matches} questões chamadas Q0;Q1;...Qn com 4 alternativas chamadas Q0a,Q0b,Q0c,Q0d para Q0; ...Qna,Qnb,Qnc,Qnd para Qn. Cada pergunta deve ter no máximo 40 tokens. Cada alternativa deve ter no máximo 15 tokens. A alternativa correta deve se chamar Q0_C para Q0; Q1_C para Q1; Qn_C para Qn. As perguntas devem ter o nível ${level}. Os temas das perguntas devem ser: ${themes.join(' T, ')}. Não retorne NADA. APENAS um array de objetos Javascript na estrutura ${JSON.stringify(DATA)} substituindo os valores correspondentes.`
   }
 }
 
-const OBJECT = {
-  "theme": "(tema1)",
-  "question": "(questão1)",
-  "answers": { "a": "(opção1)", "b": "(opção2)", "c": "(opção3)", "d": "(opção4)" },
-  "correctAlternative": "(letraCorreta)"
-};
+const DATA = [
+  {
+    question: 'Q0',
+    answers: { a: 'Q0a', b: 'Q0b', c: 'Q0c', d: 'Q0d' },
+    correct: 'Q0_C',
+    theme: 'T',
+  },
+  {
+    question: 'Qn',
+    answers: { a: 'Qna', b: 'Qnb', c: 'Qnc', d: 'Qnd' },
+    correct: 'Qn_C',
+    theme: 'T',
+  },
+];
